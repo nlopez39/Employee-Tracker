3ec7335c3526703b50d78e503e8ac30c439c488a
Nelly Lopez <nellymlopez3@gmail.com>
2024-03-07 20:08:13 -0800

    added Autoincrement

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,14 @@ create database employee_db;
 use employee_db; 
 
 create table department(
-    id INT not null, 
+    id INT not null AUTO_INCREMENT, 
     name varchar(30),
     primary key(id)
 
 );
 
 create table role(
-    id INT not null, 
+    id INT not null AUTO_INCREMENT, 
     title varchar(30),
     salary decimal, 
     department_id INT,
@@ -20,7 +20,7 @@ create table role(
 );
 
 create table employee(
-    id INT not null, 
+    id INT not null AUTO_INCREMENT,
     first_name varchar(30),
     last_name varchar(30), 
     role_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,28 @@
--- INSERT INTO department (id, name) VALUES (1, "Engineering");
--- INSERT INTO department (id, name) VALUES (2, "Finance");
--- INSERT INTO department (id, name) VALUES (3, "Legal");
--- INSERT INTO department (id, name) VALUES (4, "Sales");
+-- INSERT INTO department (name) VALUES ( "Engineering");
+-- INSERT INTO department (name) VALUES ( "Finance");
+-- INSERT INTO department (name) VALUES ("Legal");
+-- INSERT INTO department (name) VALUES ("Sales");
 
--- INSERT INTO role (id, title, salary, department_id) VALUES (1, "Sales Lead", 100000, 4 );
--- INSERT INTO role (id, title, salary, department_id) VALUES (2, "Salesperson", 80000, 4 );
--- INSERT INTO role (id, title, salary, department_id) VALUES (3, "Lead Engineer", 150000, 1);
--- INSERT INTO role (id, title, salary, department_id) VALUES (4, "Software Engineer", 120000, 1 );
--- INSERT INTO role (id, title, salary, department_id) VALUES (5, "Account Manager", 160000, 2 );
--- INSERT INTO role (id, title, salary, department_id) VALUES (6, "Accountant", 125000, 2 );
--- INSERT INTO role (id, title, salary, department_id) VALUES (7, "Legal Team Lead", 250000, 3 );
--- INSERT INTO role (id, title, salary, department_id) VALUES (8, "Lawyer", 190000, 3 );
--- a manager named Ben Schwartz
+-- INSERT INTO role ( title, salary, department_id) VALUES ( "Sales Lead", 100000, 4 );
+-- INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 80000, 4 );
+-- INSERT INTO role (title, salary, department_id) VALUES ("Lead Engineer", 150000, 1);
+-- INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 120000, 1 );
+-- INSERT INTO role (title, salary, department_id) VALUES ("Account Manager", 160000, 2 );
+-- INSERT INTO role ( title, salary, department_id) VALUES ("Accountant", 125000, 2 );
+-- INSERT INTO role ( title, salary, department_id) VALUES ("Legal Team Lead", 250000, 3 );
+-- INSERT INTO role ( title, salary, department_id) VALUES ("Lawyer", 190000, 3 );
 
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, "Ben", "Schwartz", 3, null); 
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (2, "Sarah", "Wheeler", 5, null); 
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (3, "Danielle", "Serrano", 1, null);  
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (4, "James", "Smith", 7, null); 
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (5, "Sergio", "Perez", 2, 3);  
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Ben", "Schwartz", 3, null); 
+-- INSERT INTO employee ( first_name, last_name, role_id, manager_id) VALUES ("Sarah", "Wheeler", 5, null); 
+-- INSERT INTO employee ( first_name, last_name, role_id, manager_id) VALUES ("Danielle", "Serrano", 1, null);  
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("James", "Smith", 7, null); 
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sergio", "Perez", 2, 3);  
 
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (6, "Kimberly", "Kardashian", 2,3);  
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (7, "Jackson", "Russell", 4, 1);  
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (8, "Kaia", "Gerber", 4, 1);  
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (9, "Kylie", "Jenner", 6, 2);  
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (10, "Lewis", "Hamilton", 8, 4);  
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (11, "Max", "Verstappen", 8, 4);  
+-- INSERT INTO employee ( first_name, last_name, role_id, manager_id) VALUES ( "Kimberly", "Kardashian", 2,3);  
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jackson", "Russell", 4, 1);  
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Kaia", "Gerber", 4, 1);  
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Kylie", "Jenner", 6, 2);  
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Lewis", "Hamilton", 8, 4);  
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Max", "Verstappen", 8, 4);  
 
 
