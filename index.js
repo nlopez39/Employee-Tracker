0cc5fdf3756e842ed167ef70c13f87f67d4e4268
Nelly Lopez <nellymlopez3@gmail.com>
2024-03-10 09:45:55 -0700

    added functions for add employee

--- index.js
+++ index.js
@@ -53,6 +53,92 @@ function initializePrompt() {
           initializePrompt();
         });
         break;
+      case "Add Employee":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is their first name?",
+              name: "empFirstName",
+            },
+            {
+              type: "input",
+              message: "What is their last name?",
+              name: "empLastName",
+            },
+          ])
+          .then((results) => {
+            //save the results in an array from first two questions
+            const params = [results.empFirstName, results.empLastName];
+            //then call the roles table
+            db.query("SELECT id, title FROM role", function (err, data) {
+              const role = data.map(({ id, title }) => ({
+                value: id,
+                name: title,
+              }));
+
+              //then write an inquirer prompt to then pass the roles
+              inquirer
+                .prompt([
+                  {
+                    type: "list",
+                    message: "What is their role?",
+                    name: "empRole",
+                    choices: role,
+                  },
+                ])
+                .then((result) => {
+                  const employeeRole = result.empRole;
+                  params.push(employeeRole);
+
+                  //now call the list of employees with null in them meaning they are managers
+                  db.query(
+                    "Select id, first_name, last_name from employee where manager_id is null",
+                    params,
+                    (error, data) => {
+                      const managerList = data.map(
+                        ({ id, first_name, last_name }) => ({
+                          value: id,
+                          name: first_name,
+                          last: last_name,
+                        })
+                      );
+                      //call the question
+                      inquirer
+                        .prompt([
+                          {
+                            type: "list",
+                            message: "Who is their manager?",
+                            name: "empManager",
+                            choices: managerList,
+                          },
+                        ])
+                        .then((result) => {
+                          const manager = result.empManager;
+                          params.push(manager);
+                          console.log(params);
+                          //insert the params into the employee table
+                          db.query(
+                            `INSERT INTO employee( first_name, last_name,role_id, manager_id) VALUES (?, ?, ?, ?)`,
+                            params,
+                            function (error, result) {
+                              console.log("Success!");
+                            }
+                          );
+                          db.query(
+                            "Select * from employee",
+                            function (error, result) {
+                              console.table(result);
+                              initializePrompt();
+                            }
+                          );
+                        });
+                    }
+                  );
+                });
+            });
+          });
+        break;
       case "Add Role":
         inquirer
           .prompt([
@@ -90,7 +176,8 @@ function initializePrompt() {
                   //   console.log(params);
                   //insert the params into an insert statement
                   db.query(
-                    `INSERT INTO role ( title, salary, department_id) VALUES (${params[0]}, ${params[1]}, ${params[2]});`,
+                    `INSERT INTO role ( title, salary, department_id) VALUES (?, ?, ?)`,
+                    params,
                     function (error, result) {
                       console.log("Success!");
                     }
